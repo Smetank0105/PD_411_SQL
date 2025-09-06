077a715dcd3abdfa8c251532498a567f1e0d2173
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2025-09-06 15:25:30 +0700

    HW Schedule with holidays & studyScheme DONE

--- TransactSQL/SQLQuery2-CREATE PROCEDURE HW sp_AddSchedule.sql
+++ TransactSQL/SQLQuery2-CREATE PROCEDURE HW sp_AddSchedule.sql
@@ -28,6 +28,24 @@ GO
 --	SET	@count		=	@count	+	1;
 --END
 
+--CREATE TABLE StudyScheme
+--(
+--	scheme_id	TINYINT			PRIMARY KEY,
+--	scheme_name	NVARCHAR(50)	NOT NULL,
+--	study_day	NVARCHAR(10)	NOT NULL
+--);
+
+--INSERT	StudyScheme
+--		(scheme_id, scheme_name, study_day)
+--VALUES
+--		(1,			'Even',		'Tuesday'),
+--		(2,			'Even',		'Thursday'),
+--		(3,			'Even',		'Saturday'),
+--		(4,			'Odd',		'Monday'),
+--		(5,			'Odd',		'Wednesday'),
+--		(6,			'Odd',		'Friday');
+
+
 ALTER PROCEDURE sp_AddSchedule
 	@group_name				AS	NCHAR(10),
 	@discipline_name		AS	NVARCHAR(150),
@@ -45,12 +63,12 @@ BEGIN
 
 	SET DATEFIRST	1;
 
-	DECLARE	@last_study_day_of_week	AS	INT	=	IIF(DATEPART(WEEKDAY,	@start_date)%2	=	0,	6,	5);
+	DECLARE	@study_scheme	AS	NVARCHAR(10)	=	(SELECT TOP 1 scheme_name FROM StudyScheme WHERE study_day = DATENAME(WEEKDAY, @date) ORDER BY scheme_name);
 
 	IF EXISTS (SELECT discipline FROM Schedule WHERE [group] = @group)
 	BEGIN
 		SELECT TOP 1 @date = [date] FROM Schedule WHERE [group] = @group ORDER BY [date] DESC;
-		SET	@date =	DATEADD(DAY, IIF(DATEPART(WEEKDAY, @date) = @last_study_day_of_week, 3, 2), @date);
+		SET	@date = DATEADD(DAY, IIF(DATEPART(WEEKDAY, @date)% 2 = 0, 1, 2), @date);
 	END
 
 	WHILE	@lesson_number	<=	@lessons_count
@@ -59,16 +77,19 @@ BEGIN
 		BEGIN
 			IF	EXISTS	(SELECT holiday_id FROM Holidays WHERE holiday_date = @date)
 			BEGIN
-				SET	@date =	DATEADD(DAY, IIF(DATEPART(WEEKDAY, @date) = @last_study_day_of_week, 3, 2), @date);
+				SET	@date =	DATEADD(DAY, 1, @date);
 				CONTINUE
 			END
-			INSERT	Schedule
-					([group]	,discipline,	teacher,	[date],	[time],	spent)
-			VALUES
-					(@group,	@discipline,	@teacher,	@date,	@start_time,						IIF(@date < GETDATE(), 1, 0)),
-					(@group,	@discipline,	@teacher,	@date,	DATEADD(MINUTE, 95, @start_time),	IIF(@date < GETDATE(), 1, 0));
+			IF	(DATENAME(WEEKDAY, @date) IN (SELECT study_day FROM StudyScheme WHERE scheme_name = @study_scheme))
+			BEGIN
+				INSERT	Schedule
+						([group]	,discipline,	teacher,	[date],	[time],	spent)
+				VALUES
+						(@group,	@discipline,	@teacher,	@date,	@start_time,						IIF(@date < GETDATE(), 1, 0)),
+						(@group,	@discipline,	@teacher,	@date,	DATEADD(MINUTE, 95, @start_time),	IIF(@date < GETDATE(), 1, 0));
+			SET	@lesson_number	=	@lesson_number	+	2;
+			END
 		END
-		SET	@lesson_number	=	@lesson_number	+	2;
-		SET	@date =	DATEADD(DAY, IIF(DATEPART(WEEKDAY, @date) = @last_study_day_of_week, 3, 2), @date);
+		SET	@date =	DATEADD(DAY, 1, @date);
 	END
 END
